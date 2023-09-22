4149b7a0dc2da1479c6a7d4a9458855082d889da
e <e@ii.io>
2023-09-22 18:58:49 -0400

    Fixing lastmap

--- helpers/check.go
+++ helpers/check.go
@@ -4,8 +4,10 @@ import "github.com/intangere/new_macros/core"
 import "github.com/dave/dst"
 
 func IsLast(d dst.Node, macro_name string) bool {
-	if core.IsLastMap[macro_name] == d {
-		return true
+	if n, ok := core.IsLastMap[macro_name]; ok {
+		if n == d {
+			return true
+		}
 	}
 	return false
 }
