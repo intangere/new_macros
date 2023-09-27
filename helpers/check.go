9da5479c72f8870e87ee29d5039679983a483fbe
e <e@ii.io>
2023-09-27 18:19:34 -0400

    interface checker

--- helpers/check.go
+++ helpers/check.go
@@ -1,12 +1,27 @@
 package helpers
 
 import "github.com/intangere/new_macros/core"
-//import "github.com/dave/dst"
+import "github.com/dave/dst"
 
 func IsLast(node_count int, macro_name string) bool {
 	if macro_count, ok := core.IsLastMap[macro_name]; ok {
 		if node_count == macro_count {
 			return true
+		}
+	}
+	return false
+}
+
+func InterfaceExists(pkg_name string, interface_name string) bool {
+	for _, pkg := range core.Annotated_packages {
+		if pkg.PkgName == pkg_name {
+			for _, intr := range pkg.Interfaces {
+				for _, spec := range intr.(*dst.GenDecl).Specs {
+					if spec.(*dst.ValueSpec).Names[0].String() == interface_name {
+						return true
+					}
+				}
+			}
 		}
 	}
 	return false
---
+++ macros_generated.go
@@ -0,0 +1,10 @@
+package new_macros
+
+//import "github.com/intangere/new_macros/helpers"
+
+import "go/ast"
+
+//[:kek(:zoot,5)]
+func init() {
+	var _ = (*ast.Ident)(nil)
+}
